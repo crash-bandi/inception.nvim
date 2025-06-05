06333234e1d9b51d5ac3157a2992b66d78566aaf
Jason Lines <linesj85@gmail.com>
2025-06-04 21:57:48 -0500

    portal work

--- lua/inception/init.lua
+++ lua/inception/init.lua
@@ -11,4 +11,28 @@ vim.keymap.set("n", "<leader>iwl", require("inception.ui").open_workspace)
 vim.keymap.set("n", "<C-S-Right>", require("inception.api").set_workspace_next)
 vim.keymap.set("n", "<C-S-Left>", require("inception.api").set_workspace_prev)
 
+vim.api.nvim_create_user_command('Inception', function(opts)
+  local args = vim.split(opts.args, ' ')
+  local subcommand = args[1]
+
+  if subcommand == "open_workspace" then
+    require("inception.ui").open_workspace()
+  else
+    print("Unknown subcommand: " .. (subcommand or "nil"))
+  end
+end, {
+  nargs = '*',
+  complete = function(arg_lead, cmd_line, cursor_pos)
+    local subcommands = { "open_workspace" }
+    local matches = {}
+    for _, cmd in ipairs(subcommands) do
+      if vim.startswith(cmd, arg_lead) then
+        table.insert(matches, cmd)
+      end
+    end
+    return matches
+  end,
+  desc = 'Inception commands',
+})
+
 return M
--- lua/inception/portal.lua
+++ lua/inception/portal.lua
@@ -1,5 +1,6 @@
 ---@class Inception.Portal
 ---@field windows {number: Inception.Portal.Window}
+---@field workspace_windows {number: number}
 ---@field window_state Inception.Portal.WindowState
 ---@field window_mode Inception.Portal.WindowMode
 ---@field buffer number
@@ -39,11 +40,12 @@ PortalWindow._new = {
 		col = 5,
 		style = "minimal",
 		border = "single",
-		-- hide = true,
+		hide = true,
 	},
 }
 
 Portal.windows = {}
+Portal.workspace_windows = {}
 Portal.window_state = PortalWindow.STATE.closed
 Portal.window_mode = PortalWindow.MODE.readonly
 
@@ -63,15 +65,73 @@ function Portal:buffer_load_file(path)
 	end)
 end
 
----@param wsid number
-function Portal:window_create(wsid)
+---@param workspace Inception.Workspace
+function Portal:window_create(workspace)
+  if not workspace.attachment then
+    error("Cannot create a portal window for a detached workspace")
+  end
+
 	local window = setmetatable(vim.deepcopy(PortalWindow._new), PortalWindow)
 
-	window.id = wsid
-	window.workspace = wsid
+  local tabpage = nil
+  if workspace.attachment.type == "tab" then
+    tabpage = workspace.attachment.id
+  elseif workspace.attachment.type == "win" then
+    tabpage = vim.api.nvim_win_get_tabpage(workspace.attachment.id)
+  else
+    error("Invalid workspace attachment type: " .. workspace.attachment.type)
+  end
+
+  if tabpage ~= vim.api.nvim_get_current_tabpage() then
+    ---TODO: figure out if a sneaky enter/create/exit can be performed here, or only call on the first workspace enter event
+    error("Cannot create a portal window on an inactive workspace")
+  end
+
+	window.id = vim.api.nvim_open_win(Portal.buffer, true, window.opts)
+	window.workspace = workspace.id
 
 	self.windows[window.id] = window
+  self.workspace_windows[window.workspace] = window.id
 	return window.id
+end
+
+---@param winid number
+function Portal:window_close(winid)
+  local window = self:get_window(winid)
+  self.workspace_windows[window.workspace] = nil
+  self.windows[window.id] = nil
+end
+
+---@param winid number
+---@return Inception.Portal.Window
+function Portal:get_window(winid)
+  local window = self.windows[winid]
+
+  if not window then
+    error("Invalid window id: " .. winid)
+  end
+
+  return window
+end
+
+---@param wsid number
+---@return number
+function Portal:get_workspace_window(wsid)
+  local winid = self.workspace_windows[wsid]
+
+  if not winid then
+    error("No window found for workspace " .. wsid)
+  end
+
+  return winid
+end
+
+---@param wsid number
+---@return boolean
+function Portal:get_workspace_has_window(wsid)
+  local ok, _ = pcall(self.get_workspace_has_window, self, wsid)
+
+  return ok
 end
 
 function PortalWindow:update()
@@ -87,19 +147,11 @@ function PortalWindow:update()
 end
 
 function PortalWindow:_open()
-	if self.winid then
-		return
-	end
-	print("opening portal window " .. self.id)
-	self.winid = vim.api.nvim_open_win(Portal.buffer, true, self.opts)
+  vim.api.nvim_win_set_config(self.id, {hide = false})
 end
 
 function PortalWindow:_close()
-	if not self.winid then
-		return
-	end
-
-	vim.api.nvim_win_close(self.winid, false)
+  vim.api.nvim_win_set_config(self.id, {hide = true})
 end
 
 return Portal
